676ba4fde6e7e0c28387fc5b9ab034f34cb439ed
Antonio David <asanchezcarrillo658@gmail.com>
2025-05-12 10:44:49 +0200

    Contador

---
+++ docs/js/app.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    const weddingDate = new Date("2025-09-10T00:00:00");
+    const now = new Date();
+    const diff = weddingDate - now;
+
+    if (diff <= 0) {
+        document.getElementById("countdown").innerHTML = "<p>¡Ya es el gran día!</p>";
+        return;
+    }
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    document.getElementById("days").textContent = days.toString().padStart(2, '0');
+    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown()
